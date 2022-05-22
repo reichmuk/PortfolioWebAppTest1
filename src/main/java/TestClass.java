05bb7177e42993dd6ac11268ee7eae21e364cff9
kevin.reichmuth <reichkev@students.zhaw.ch>
2022-05-22 19:48:52 +0200

    slight update

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,9 +2,9 @@ public class TestClass {
 
     public static void main(String[] args){
 
-            //ConnectionTest connectionTest = new ConnectionTest();
-            YahooApi yahooApi = new YahooApi();
-            yahooApi.testAPI("UBSG.SW");
+            ConnectionTest connectionTest = new ConnectionTest();
+            //YahooApi yahooApi = new YahooApi();
+            //yahooApi.testAPI("UBSG.SW");
             //yahooApi.testAPI2();
 
     }
